140ff721ae5bee6aec513fd9136a65dd69481984
Uladzimir <u.hanczar@gmail.com>
2017-03-30 15:32:19 +0300

    6.Chaining_app_before_promices

--- wether_app/app.js
+++ wether_app/app.js
@@ -18,12 +18,17 @@ const argv = yargs
 const address = argv.address;
 console.log(address);
 
-// geocode.geocodeAddress(address, (errorMessage, results) => {
-//     if (errorMessage) {
-//         console.log(errorMessage);
-//     } else {
-//         console.log(JSON.stringify(results, undefined, 2));
-//     }
-// });
-
-weather.getWeather();
+geocode.geocodeAddress(address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(results.address);
+        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+          if(errorMessage) {
+            console.log(errorMessage);
+          } else {
+            console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}`);
+          }
+        });
+    }
+});
--- wether_app/weather/weather.js
+++ wether_app/weather/weather.js
@@ -1,14 +1,18 @@
 const request = require('request');
 
-const getWeather = () => {
+const getWeather = (latitude, longitude, callback) => {
   return request({
-    url: "https://api.darksky.net/forecast/1130bde1c60eef211aed30d9eb30c153/52.4411761,30.9878462",
+    url: `https://api.darksky.net/forecast/1130bde1c60eef211aed30d9eb30c153/${latitude},${longitude}`,
     json: true
   }, (error, responce, body) => {
-    if (!error && responce.statusCode === 200) {
-      console.log(body.currently.temperature);
+    if (error) {
+      callback('Unable to connect to Forecast.io server.');
     } else if (responce.statusCode === 404) {
-      console.log('Unable to fetch weather');
+      callback('Unable to fetch weather');
+    } else if (!error && responce.statusCode === 200) {
+      callback(undefined, {
+        temperature: body.currently.temperature, apparentTemperature: body.currently.apparentTemperature
+      });
     }
   });
 };
