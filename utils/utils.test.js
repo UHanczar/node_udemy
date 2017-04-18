e527751dea28c32c5bb333669ab39d4f3578d1a3
UHanczar <u.hanczar@gmail.com>
2017-04-18 17:09:36 +0300

    11.Agging_expect

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,21 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('it should add two numbers', () => {
   const res = utils.add(33, 11);
-  if(res !== 44) {
-    throw Error(`Expected 44, but got ${res}.`);
-  }
+  // if(res !== 44) {
+  //   throw Error(`Expected 44, but got ${res}.`);
+  // }
+
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('it should square the number', () => {
   const res = utils.square(5);
-  if(res !== 25) {
-    throw Error(`Expected 25, but got ${res}.`);
-  }
+  // if(res !== 25) {
+  //   throw Error(`Expected 25, but got ${res}.`);
+  // }
+
+  expect(res).toBe(25).toBeA('number');
 });
